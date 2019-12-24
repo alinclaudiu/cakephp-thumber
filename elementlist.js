5770e0e3e987c54a7a9b7021b014f45a06ad0185
mirko-pagliai <mirko.pagliai@gmail.com>
2019-12-24 14:00:55 +0100

    updated

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Thumber\\Command\\ClearAllCommand"],["c","Thumber\\Command\\ClearCommand"],["c","Thumber\\Http\\Exception\\ThumbNotFoundException"],["c","Thumber\\Plugin"],["c","Thumber\\Routing\\Middleware\\ThumbnailMiddleware"],["c","Thumber\\TestSuite\\IntegrationTestTrait"],["c","Thumber\\TestSuite\\TestCase"],["c","Thumber\\ThumbsPathTrait"],["c","Thumber\\Utility\\ThumbCreator"],["c","Thumber\\Utility\\ThumbManager"],["c","Thumber\\View\\Helper\\ThumbHelper"]];
